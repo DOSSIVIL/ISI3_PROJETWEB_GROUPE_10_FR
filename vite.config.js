a21f459d80bf4fd3e070a3d4777219a426ac8ad1
DOSSIVIL <dossivil@gmail.com>
2026-01-14 07:30:58 +0100

    config vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-export default {
-  server: {
-    hmr: {
-      overlay: false
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
     }
+  },
+  server: {
+    port: 3000
   }
-}
+});
